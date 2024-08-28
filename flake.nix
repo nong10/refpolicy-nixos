9623b578807aee131f829b86b3db3316cc707efb
nong10 <nong10w@outlook.com>
2024-08-28 09:46:18 +0200

    make flags

--- flake.nix
+++ flake.nix
@@ -12,12 +12,62 @@
   {
     packages.${system}.default = mkDerivation {
       name = "refpolicy";
-      src = fetchFromGitHub {
-        owner = "SELinuxProject";
-        repo = "refpolicy";
-        rev = "RELEASE_2_20240226";	  # a tag
-        hash = "SHA-256:7ed41f4f45189b9ee9706da8ac357eccc103651b56daabaddb54c436e8117cf9";
+
+      src = fetchzip {
+        url = "https://github.com/SELinuxProject/refpolicy/releases/download/RELEASE_2_20240226/refpolicy-2.20240226.tar.bz2";
+        hash = "sha256-C0aBjjO7YBi5atl/mmBP1l3fLhtsG/xwFXUMlz20smM=";
+        stripRoot = false;
+        nativeBuildInputs = [ bzip2 gnutar ];
       };
+
+      makefile = "Makefile";
+
+      makeFlags = [
+        "topdir=${out}"
+        "AWK=${pkgs.gawk}/bin/awk "
+        "GREP=${pkgs.gnugrep}/bin/grep -E "
+        "INSTALL=${pkgs.coreutils}/bin/install "
+        "M4=${pkgs.gnum4}/bin/m4 -E -E "
+        "PYTHON=${pkgs.python311}/bin/python3 -bb -t -t -E -W error"
+        "SORT=LC_ALL=C ${pkgs.coreutils}/bin/sort"
+        "SED=${pkgs.gnused}/bin/sed "
+        "CHECKPOLICY=${pkgs.checkpolicy}/bin/checkpolicy "
+        "CHECKMODULE=${pkgs.checkpolicy}/bin/checkmodule"
+        "SEMODULE=${pkgs.policycoreutils}/bin/semodule"
+        "SEMOD_PKG=${pkgs.semodule-utils}/bin/semodule_package"
+        "SEMOD_LNK=${pkgs.semodule-utils}/bin/semodule_link"
+        "SEMOD_EXP=${pkgs.semodule-utils}/bin/semodule_expand"
+        "LOADPOLICY=${pkgs.policycoreutils}/bin/load_policy"
+        "SEPOLGEN_IFGEN=${pkgs.selinux-python}/bin/sepolgen-ifgen"
+        "SETFILES=${pkgs.policycoreutils}/bin/setfiles"
+        "SEFCONTEXT_COMPILE=${pkgs.libselinux}/bin/sefcontext_compile"
+        "SECHECK=${pkgs.setools}/bin/sechecker"
+        "XMLLINT=${pkgs.libxml2}/bin/xmllint"
+      ];
+
+      buildPhase = ''
+        make install-src
+        cd ${out} 
+        make conf
+      '';
+
+
+
+      nativeBuildInputs = [
+        git
+        gawk	      # awk
+        gnugrep	    # grep
+        coreutils	  # GNU coreutils: install sort
+        gnused	    # sed
+        gnumake	    # make
+        gnum4	      # m4
+        policycoreutils # semodule
+        checkpolicy     # checkpolicy checkmodule
+        semodule-utils  # semodule_link semodule_unpackage semodule_expand semodule_package
+        setools         # sechecker
+        (libselinux.override { enablePython = true; } )      # sefcontext_compile
+        libxml2         # xmllint
+      ];
     };
   };
 
